3317446df505732d6063473bb5e99a37d3847089
cici755 <cicichuanzhang@gmail.com>
2026-07-07 20:19:31 +0800

    Add files via upload

---
+++ _next/static/tqRuq1ZWeNdh4D6kW9OAu/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/MyDiary-Static//_next/:path+",
+        "destination": "/MyDiary-Static/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
